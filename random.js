3b3e357bbee06f6b19690a6f97eea385f74f8b2a
jjuriz <jjuriz@gmail.com>
2020-05-02 18:20:06 -0400

    Added Clear button to remove results.

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 let numberOfDiceInput = document.getElementById("number_of_dice"),
     rollButton = document.getElementById("roll_button"),
     numberOfSidesInput = document.getElementById("number_of_sides"),
-    displayNumbers = document.getElementById("display_numbers");
+    displayNumbers = document.getElementById("display_numbers"),
+    clearButton = document.getElementById("clear_button"),
+    numberOfRolls = 0;
 
 //Generates a random number
 let getRandomNumber = function getRandomNumber(min, max) {
@@ -18,8 +20,18 @@ rollButton.addEventListener("click", function () {
     let dieSides = numberOfSidesInput.value;
     for (let i = 0; i < numDice; i++) {
         if (i < numDice) {
-            displayNumbers.innerHTML += getRandomNumber(1, dieSides) + "<br>";
+            displayNumbers.innerHTML += `The #${i + 1} die is ${getRandomNumber(1, dieSides)} <br>`;
         }
     }
 });
 
+clearButton.addEventListener("click", function(){
+    displayNumbers.innerHTML = " ";
+})
+
+
+//Next steps:
+//Track number of rolls.
+//Record number of rolls above each result.
+//Re-roll button which rolls the specified dice and replaces the last result. 
+//CSS work.
